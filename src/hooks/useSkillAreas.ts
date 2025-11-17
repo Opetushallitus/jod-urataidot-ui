7b2a45585d970c1c16dbe31406424d087a81df99
Jussi Sirén <jussi.siren@sleek.works>
2025-11-17 13:03:48 +0200

    OPHJOD-2700: Fix language change

--- src/hooks/useSkillAreas.ts
+++ src/hooks/useSkillAreas.ts
@@ -8,22 +8,25 @@ import { useTranslation } from 'react-i18next';
  * Hook that returns a list of skill areas from content with current version and language
  */
 const useSkillAreas = (): SkillArea[] => {
-  const { t, i18n } = useTranslation();
-  const language = i18n.language as LangCode;
-  const contentRef = React.useRef<Content>(getContents(t, language).slice(-1)[0]);
-  const versionInSession = window.sessionStorage.getItem('content-version');
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+
+  const contentRef = React.useRef<Content>(getContents(t, language as LangCode).at(-1));
+  const versionInSession = globalThis.sessionStorage.getItem('content-version');
   const version = React.useMemo(() => (versionInSession ? Number(versionInSession) : null), [versionInSession]);
 
   React.useEffect(() => {
     if (typeof version === 'number') {
-      contentRef.current = getContents(t, language)[version - 1];
+      contentRef.current = getContents(t, language as LangCode)[version - 1];
     } else {
       // If no version is specified, use latest
-      contentRef.current = getContents(t, language).slice(-1)[0];
+      contentRef.current = getContents(t, language as LangCode).at(-1);
     }
   }, [version, t, language]);
 
-  return contentRef.current?.skillAreas ? contentRef.current.skillAreas : [];
+  return contentRef.current?.skillAreas ?? [];
 };
 
 export default useSkillAreas;
